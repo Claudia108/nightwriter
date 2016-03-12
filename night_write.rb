b6e4e48af07d9782d547628ee3af20e4abc7004f
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-03-12 16:40:42 -0700

    number translation working

--- night_write.rb
+++ night_write.rb
@@ -4,69 +4,110 @@ require 'pry-nav'
 
 class NightWrite
 
-  def english_letter_translate(letter)
-    letter_map = Letter.new
-    letter_map.english_to_braille[letter]
+  def initialize
+    @letter_map = Letter.new
+    @count = 0
+  end
+
+  # def english_letter_translate(letter, num=false)
+  #   translation = []
+  #   if num == true
+  #     letter.first.each_char do |char|
+  #       translation << @letter_map.english_to_braille[char]
+  #     end
+  #     translation
+  #   else
+  #     @letter_map.english_to_braille[letter]
+  #   end
+  # end
+
+  def english_letter_translate(char)
+      @letter_map.english_to_braille[char.downcase]
   end
 
   def is_upcase?(letter)
-    symbols = [" ", "!", "'", "-", ".", ",", "?",
+    symbols = [" ", "!", "'", "-", ".", ",", "?", "#", "&",
               "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     letter == letter.upcase && !symbols.include?(letter)
   end
+
+  # def support_upcase(braille_letter)
+  #   braille_letter = english_letter_translate("Cap")
+  #   load_braille_letters(braille_message, braille_letter)
+  #   binding.pry
+  # end
 
   def is_number?(number)
     ("0".."9").to_a.include?(number)
   end
 
-  def support_numbers(number, braille_message, braille_letters)
-    braille_letters = english_letter_translate("#")
-    load_braille_letters(braille_message, braille_letters)
-    is_number? = false
-    braille_letters = english_letter_translate(char.downcase)
-    load_braille_letters(braille_message, braille_letters)
+  # def map_numbers_with_index(characters)
+  #   characters.chars.map.with_index do |char, index|
+  #    index if is_number?(char)
+  #   end.compact
+  #   # indexes.compact
+  # end
 
-    braille_letters = english_letter_translate(" ")
-    end
-    load_braille_letters(braille_message, braille_letters)
+  def support_numbers(message)
     @count += 2
+    message.gsub(/(\d+)/, '#\1 ')
   end
 
-    # input: number
-    # output: boolean
+  def support_upcase(message)
+    message.chars.map do |char|
+      if is_upcase?(char)
+        @count += 1
+        char.gsub(char, "&#{char.downcase}")
+      else
+        char
+      end
+    end.join
+  end
+
+  def upcase_and_number_string(chars)
+    with_upcase = support_upcase(chars)
+    support_numbers(with_upcase)
+  end
 
   def load_braille_letters(braille_message, braille_letters)
+    # binding.pry
     braille_message[0] << braille_letters[0]
     braille_message[1] << braille_letters[1]
     braille_message[2] << braille_letters[2]
   end
 
-  def message_translate(message)
+  def message_translate(chars)
     braille_message = [[],[],[]]
-    @count = 0
-    message.each_char do |char|
-      if is_upcase?(char)
-        braille_letters = english_letter_translate("Cap")
-        load_braille_letters(braille_message, braille_letters)
-        # binding.pry
-        @count += 1
-      elsif is_number?(char)
-        braille_letters = english_letter_translate("#")
-        load_braille_letters(braille_message, braille_letters)
-        is_number? = false
-        braille_letters = english_letter_translate(char.downcase)
-        load_braille_letters(braille_message, braille_letters)
-
-        braille_letters = english_letter_translate(" ")
-        end
-        load_braille_letters(braille_message, braille_letters)
-        @count += 2
-      end
-      braille_letters = english_letter_translate(char.downcase)
+    upcase_and_number_string(chars).each_char do |char|
+      braille_letters = english_letter_translate(char)
+      # binding.pry
       load_braille_letters(braille_message, braille_letters)
     end
+    # binding.pry
     braille_message
   end
+
+
+  # def message_translate(message)
+  #   braille_message = [[],[],[]]
+  #   @count = 0
+  #   message.each_char do |char|
+  #     if is_upcase?(char)
+  #       braille_letters = english_letter_translate("Cap")
+  #       load_braille_letters(braille_message, braille_letters)
+  #       @count += 1
+  #     elsif is_number?(char)
+  #       num = support_numbers(char)
+  #       # binding.pry
+  #       braille_letters = english_letter_translate(num, true)
+  #       load_braille_letters(braille_message, braille_letters)
+  #       @count += 2
+  #     end
+  #     braille_letters = english_letter_translate(char.downcase)
+  #     load_braille_letters(braille_message, braille_letters)
+  #   end
+  #   braille_message
+  # end
 
   def line_break(braille_message)
     line = braille_message
--- night_write_test.rb
+++ night_write_test.rb
@@ -4,52 +4,92 @@ require_relative 'night_write'
 
 class NightWriteTest < Minitest::Test
 
- def test_it_translates_english_letter_into_braille_letters
+  def test_english_letter_translate_matches_an_english_letter_with_braille_letter
    letter = NightWrite.new
+   letter_a = {"a" => ["0.","..",".."]}
    assert_equal ["0.","..",".."], letter.english_letter_translate("a")
- end
+  end
 
- def test_it_returns_true_if_letter_is_capital_and_not_a_symbol
+  def test_is_upcase_returns_true_if_letter_is_capital_and_not_a_symbol_or_number
    letter = NightWrite.new
    assert letter.is_upcase?("A")
    refute letter.is_upcase?("!")
- end
+   refute letter.is_upcase?("2")
+  end
 
- def test_it_inserts_a_shift_character_for_capital_letter
+  def test_support_upcase_inserts_shift_character_in_front_of_letter
    letter = NightWrite.new
+   shift_character = {"Cap" => ["..","..",".0"]}
+   character = {"a" => ["0.","..",".."]}
    assert_equal [["..", "0."], ["..", ".."], [".0", ".."]], letter.message_translate("A")
- end
+  end
 
- def test_it_prints_braille_letters_next_to_each_other
+  def test_is_number_returns_true_if_character_is_number
+    message = NightWrite.new
+    number = "8"
+    assert_equal true, message.is_number?(number)
+  end
+
+  def test_support_numbers_inserts_shift_character_in_front_of_number
+    skip
+    letter = NightWrite.new
+    # For Reference:
+    # shift_character = {"#" => [".0",".0","00"]}
+    # character = {"1" => ["0.","..",".."]}
+    assert_equal [[".0", "0."], [".0", ".."], ["00", ".."]], letter.message_translate("77")
+  end
+
+  def test_map_numbers_with_index_indexes_numbers
+    skip
+    letters = NightWrite.new
+    letter_characters = {
+      "a" => ["0.","..",".."],
+      "b" => ["0.","0.",".."]
+    }
+
+    number_characters = {
+      "1" => ["0.","..",".."],
+      "2" => ["0.","0.",".."],
+      "3" => ["00","..",".."]
+    }
+    assert_equal [2, 4, 5], letters.map_numbers_with_index("ab2a31")
+  end
+
+  def test_support_numbers_adds_pound_char_before_first_letter_and_space_behind_last
+    letters = NightWrite.new
+    assert_equal ["a#23 a#1 a"], letters.support_numbers("a23a1a")
+  end
+
+  def test_upcase_and_number_string_inserts_all_special_characters_in_string
+    letters = NightWrite.new
+    assert_equal ["a#23 Capa#1 a"], letters.support_numbers("a23A1a")
+  end
+
+  def test_load_braille_letters_collects_each_part_of_braille_letter_in_seperate_array
+
+  end
+
+  def test_message_translate_converts_normal_letters_into_braille_letters
+
+  end
+
+  def test_line_break_breaks_the_line_after_80_characters
+    skip
+    line = NightWrite.new
+    assert_equal "#{line}\n", line.line_break(80)
+  end
+
+  def test_print_braille_prints_braille_letters_next_to_each_other
    message = NightWrite.new
-   assert_equal [["0.", "0.", "0.", "0."], ["..", "0.", "..", ".."], ["..", "..", "..", ".."]], message.message_translate("abaa")
- end
+   letter_a = {"a" => ["0.","..",".."]}
+   letter_b = {"b" => ["0.","0.",".."]}
+   assert_equal [["0.", "0.", "0.", "0."],
+                 ["..", "0.", "..", ".."],
+                 ["..", "..", "..", ".."]], message.message_translate("abaa")
+  end
 
- def test_it_returns_a_string_printed_on_three_lines
-   skip
-   message = NightWrite.new
-   assert_equal "0.0.0.\n..0...\n......", message.print_braille("aba")
- end
 
- def test_it_breaks_the_line_after_40_characters
-   skip
-   line = NightWrite.new
-   assert_equal "#{line}\n", line.line_break(40)
- end
-
- def test_it_prints_an_additional_character_before_each_capital_letter
-   skip
-   message = NightWrite.new
-   assert_equal "..0.\n....\n.0..", message.print_braille("A")
- end
-
- def test_is_number_checks_for_number_character
-   message = NightWrite.new
-   number = "8"
-   assert_equal true, message.is_number?(number)
- end
-
- def test_support_numbers_supports_the_translation_of_number_with_surrounding_characters
+  def test_support_numbers_writes_number
    skip
    message = NightWrite.new
    number = "5"
@@ -68,7 +108,7 @@ class NightWriteTest < Minitest::Test
      "22225555"
    ]
 
-   assert_equal
+
    # input english numbers
    # map those with braille letters
    # add special character (#) in front and space in back
@@ -78,6 +118,14 @@ class NightWriteTest < Minitest::Test
    # keep track of length of numbers
    # after last add space
 
- end
+  end
+
+  def test_it_can_translate_a_string_of_numbers
+   skip
+   message = NightWrite.new
+   english_letters = "77"
+   braille_letters = [[".0",".0","00"],["0.","..",".."], ["0.","0.",".."],["..","..",".."]]
+   assert_equal braille_letters, message.message_translate(english_letters)
+  end
 
 end
